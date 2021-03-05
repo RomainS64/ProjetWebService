c8f1dc70a1ab0a382bd039599e75063cfee031b1
Romain Salha <rsalha@iutbayonne.univ-pau.fr>
2021-03-05 16:54:42 +0100

    CSS et finalisation de la page d'accueil + creation de resultat.php

--- resultat.php
+++ resultat.php
@@ -6,9 +6,16 @@
   <link href="style.css" rel="stylesheet">
   <title>SunnyPath</title>
 <body>
-  <h1>ville:<?php echo($_POST['ville']);?></h1>
-  <h1>locomotion:<?php echo($_POST['locomotion']);?></h1>
-  <h1>heure:<?php echo($_POST['heure']);?></h1>
+  <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <p>Trajet pour aller à <?php echo($_POST['ville']);?> en <?php echo($_POST['locomotion']);?></p>
+    </div>
+    <div class="col-sm">
+      <p>Météo à <?php echo($_POST['ville']);?></p>
+    </div>
+  </div>
+</div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
